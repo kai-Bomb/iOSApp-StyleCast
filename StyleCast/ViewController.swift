8c509e9ce684d60b65205a274a553ed8f406b02e
kai-Bomb <katsumasa.watanabe@zozo.com>
2024-11-07 18:05:35 +0900

    Initial Commit

---
+++ StyleCast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StyleCast
+//
+//  Created by katsumasa.watanabe on 2024/11/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StyleCastTests/StyleCastTests.swift
@@ -0,0 +1,17 @@
+//
+//  StyleCastTests.swift
+//  StyleCastTests
+//
+//  Created by katsumasa.watanabe on 2024/11/07.
+//
+
+import Testing
+@testable import StyleCast
+
+struct StyleCastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
