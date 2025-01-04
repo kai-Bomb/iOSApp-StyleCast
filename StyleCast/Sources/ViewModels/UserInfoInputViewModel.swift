593c69c97c71fa111895cf89184ffe363316e9de
KyusokLee <kyusok1@naver.com>
2025-01-04 16:41:20 +0900

    [WIP-Add] - add UserData to CoraDataStorage

--- StyleCast/Sources/ViewModels/UserInfoInputViewModel.swift
+++ StyleCast/Sources/ViewModels/UserInfoInputViewModel.swift
@@ -6,7 +6,16 @@ class UserInfoInputViewModel: ObservableObject {
     @Published var age: String = ""
     @Published var selectedGender: Gender? = nil
 
+    private let coreDataStorage = CoreDataStorage()
+
     func register() {
-        // ToDo: CoreDataに保存する処理を書く
+        // TODO: - CoreDataに保存する処理を書く
+        let context = coreDataStorage.saveContext()
+
+        // 新しい User エンティティを作成
+        let newUser = User(context: context)
+        newUser.username = username
+        newUser.age = age
+        newUser.selectedGender = selectedGender?.rawValue ?? ""
     }
 }
